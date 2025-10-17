eb852fdaff3b11afa8ce7c88f084e250eea04d5f
iancorbett <iancorbett1324@gmail.com>
2025-10-17 13:07:27 -0700

    create function that handles upload of csv file and creates a file path to store the uploaded csv file

--- project2.php
+++ project2.php
@@ -3,6 +3,15 @@
 $uploadDir = __DIR__ . '/uploads'; //make uploaded files vieable in a folder called uploads
 $jsonTxtPath = __DIR__ . '/output.txt'; //write json string to a txt file
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['csv_file'])) { //if post request is made and we have a valid csv file
+
+    if (!is_dir($uploadDir)) mkdir($uploadDir, 0777, true); //if uploaddir hasnt been created, create it and use 0777 to grant alll users permission to read, write, execute
+
+    $tmpName = $_FILES['csv_file']['tmp_name']; //tmpname refers to temporary file path automatically assigned by php
+    $fileName = basename($_FILES['csv_file']['name']); //original filename user uploaded
+    $targetPath = $uploadDir . '/' . $fileName; //use "." to concatenate upload directory path and file name, final destination of the uploaded file
+}
+
 ?>
 
 <!DOCTYPE html>
