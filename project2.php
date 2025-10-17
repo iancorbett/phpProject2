bf42a5d98b8d4ec6ebcdceb72a221dd8efe63b3b
iancorbett <iancorbett1324@gmail.com>
2025-10-17 12:35:31 -0700

    create html form and only allow files with .csv

--- project2.php
+++ project2.php
@@ -10,6 +10,13 @@
     <title>Project #2 – CSV to JSON</title>
 </head>
 <body>
+<h1>Project #2 – CSV → JSON Converter</h1>
+
+<form method="post" enctype="multipart/form-data">
+  <p><label>Select a CSV file to upload:</label></p>
+  <input type="file" name="csv_file" accept=".csv" required>
+  <button type="submit">Upload & Convert</button>
+</form>
     
 </body>
 </html>
